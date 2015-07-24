22d42fcf855b8ef2bb56e1997d4ad818ea1edc69
Mattburford <Matthew@burfords.org>
2015-07-24 09:41:44 +0100

    Update module3.js

--- module3.js
+++ module3.js
@@ -11,7 +11,7 @@ class ImageUtils {
         var c = ImageUtils.getCanvas(img.width, img.height);
         var ctx = c.getContext('2d');
         ctx.drawImage(img, 0, 0);
-        return ctx.getImageData(0,0,c.width,c.height);
+        return ctx.getImageData(0, 0, c.width, c.height);
     }
 
     static putPixels(imageData, w, h) {
@@ -20,73 +20,7 @@ class ImageUtils {
         ctx.putImageData(imageData, 0, 0);
     }
 
-    // new in module 3
-    static fromImageData(imageData) {
-        var width = imageData.width;
-        var height = imageData.height;
-        var data = imageData.data;
-
-        var pixelGrid = initializePixelGrid(height);
-
-        for(var y = 0; y < height; y++) {
-            var row = (y * width * 4);
-
-            for(var x = 0; x < width; x++) {
-                var index = row + (x * 4);
-                var rgba = new RGBA(data[index], data[index+1], data[index+2], data[index+3]);
-                pixelGrid[y][x] = rgba;
-            }
-        }
-        return new ImageModel(height, width, pixelGrid);
-    }
-
-    // new in module 3
-    static fromImgSrc(imgSrc) {
-        var img = new Image();
-        img.src = imgSrc;
-        var data = ImageUtils.getPixels(img);
-        return ImageUtils.fromImageData(data);
-    }
-
-    // new in module 3
-    static toImageData(imageModel) {
-
-        console.log(imageModel);
-        var buffer = new ArrayBuffer(imageModel.height * imageModel.width * 4);
-
-        var data = new Uint8ClampedArray(buffer);
-
-        for(var y = 0; y < imageModel.height; y++) {
-            var row = (y * imageModel.width * 4);
-
-            for(var x = 0; x < imageModel.width; x++) {
-                var rgba = imageModel.pixelGrid[y][x];
-
-                var index =  row + (x * 4);
-
-                data[index] = rgba.red;
-                data[index + 1] = rgba.green;
-                data[index + 2] = rgba.blue;
-                data[index + 3] = rgba.alpha;
-            }
-        }
-
-        return new ImageData(data, imageModel.width, imageModel.height);
-    }
-
-    // new in module 3
-    static drawImageModel(imageModel) {
-        var c = ImageUtils.getCanvas(imageModel.width, imageModel.height);
-        var imageData = ImageUtils.toImageData(imageModel);
-        ImageUtils.putPixels(imageData, imageModel.width, imageModel.height);
-    }
-
 }
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 
 class RGBA {
     constructor(redValue, greenValue, blueValue, alphaValue) {
@@ -96,14 +30,105 @@ class RGBA {
         this.alpha = alphaValue;
     }
 }
+class Point {
+    constructor(xValue, yValue) {
+        this.x = xValue;
+        this.y = yValue;
+    }
+}
+var point = new Point(1, 2);
+console.log(point);
 
+var color = new RGBA(255, 0, 0, 1);
+console.log(red);
 
-// class definitions here
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-$(document).ready(function() {
+// function definitions here
+
+$(document).ready(function () {
     var img = new Image();
     img.src = "img/cat.jpg";
+});
 
+function sepiaPixel(colour) {
 
+    var modifiedRed = colour.red * 0.393 + colour.green * 0.769 + colour.blue * 0.189
+    var modifiedGreen = colour.red * 0.349 + colour.green * 0.686 + colour.blue * 0.168
+    var modifiedBlue = colour.red * 0.272 + colour.green * 0.534 + colour.blue * 0.131
 
+    return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, colour.alpha);
+}
+
+function sepia(img) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var all = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < all; i += 4) {
+        var originalRGBA = new RGBA(data[i], data[i + 1], data[i + 2], data[i + 3]);
+        var sepiaRGBA = sepiaPixel(originalRGBA);
+
+        data[i] = sepiaRGBA.red;
+        data[i + 1] = sepiaRGBA.green;
+        data[i + 2] = sepiaRGBA.blue;
+        data[i + 3] = sepiaRGBA.alpha;
+    }
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function colourise(img, colour, level) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var all = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < all; i += 4) {
+        var currentRGBA = new RGBA(data[i], data[i + 1], data[i + 2], data[i + 3]);
+        var modifiedRGBA = colourisePixel(currentRGBA, colour, level);
+        setPixel(data, i, modifiedRGBA);
+    }
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function colourisePixel(originalRGBA, colour, level) {
+
+    var diffRed = (originalRGBA.red - colour.red) * (level / 100);
+    var modifiedRed = originalRGBA.red - diffRed;
+
+    var diffGreen = (originalRGBA.green - colour.green) * (level / 100);
+    var modifiedGreen = originalRGBA.green - diffGreen;
+
+    var diffBlue = (originalRGBA.blue - colour.blue) * (level / 100);
+    var modifiedBlue = originalRGBA.blue - diffBlue;
+
+    return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, colour.alpha);
+}
+function clipPixel(colour, range) {
+    var clippedRed = 0;
+
+    if (colour.red > 255 - range) {
+        clippedRed = 255;
+    }
+    var clippedGreen = 0;
+    if (colour.green > 255 - range) {
+        clippedGreen = 255;
+    }
+    var clippedBlue = 0;
+    if (colour.blue > 255 - range) {
+        clippedBlue = 255;
+    }
+    return new RGBA(clippedRed, clippedGreen, clippedBlue, colour.alpha);
+}
+$(document).ready(function() {
+
+    var img = new Image();
+    img.src = "img/Golden.jpg";
+
+    colourise(img, new RGBA(150, 100, 190, 1), 50);
 });
